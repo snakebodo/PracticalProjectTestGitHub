05948c67f94e3395cacebbdb1f7bc546e5d35731
bogdan <bogdanhalici1989@gmail.com>
2023-03-09 18:47:08 +0000

    PTG-1 print preiodically current date and time on 5s intervals

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,7 +1,22 @@
 package Main;
 
+import java.time.Instant;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        printPeriodicallyCurrentDateAndTime();
+    }
+
+    public static void printPeriodicallyCurrentDateAndTime() {
+        while (true) {
+            try {
+                System.out.println(Instant.now());
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
